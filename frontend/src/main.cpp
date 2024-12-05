4404ee8093d94a28ed7460ab18db30fbebc1a364
Autzoko <autzokoLang@outlook.com>
2024-12-04 23:34:51 -0500

    build frontend, cmake errored, seems MOC problems

--- frontend/src/main.cpp
+++ frontend/src/main.cpp
@@ -1,7 +1,11 @@
 #include <QApplication>
+#include "SynthesizerUI.h"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
+
+    SynthesizerUI ui;
+    ui.show();
 
     return app.exec();
 }
